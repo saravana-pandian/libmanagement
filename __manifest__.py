6e6b058a288f165a58ec49b2b2ddc331096d7452
saravana-pandian <saravanaoff2000@gmail.com>
2021-05-27 17:32:16 +0530

    done

---
+++ __manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Library Management",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        This Library module is for odoo technical training
+    """,
+
+    'author': "__s_a_r_v_a_n_a___",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+    'views/library.xml'
+        
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+    ],
+}
---
+++ models/library.py
@@ -0,0 +1,16 @@
+
+from odoo import models, fields, api, exceptions
+
+
+
+class Library(models.Model):
+	_name = 'library.books'
+
+	_description = 'Brussels Library'
+
+	name = fields.Char(string="ISBN", required=False)
+	year = fields.Char(string="Year of Edition", required=False)
+	book_name = fields.Char(string="Book Title", required=False)
+
+	librarian_id = fields.Many2one('res.users', string="Library Admin", index=True)
+	
